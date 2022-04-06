a74fcf631aca0e7163a3e33baf1726ac47d5c634
lemaiwo <wouterlemaire120@hotmail.com>
2022-04-07 00:29:43 +0200

    TypeScript dialog

--- src/controller/App.controller.ts
+++ src/controller/App.controller.ts
@@ -14,30 +14,40 @@ import BaseController from "./BaseController";
  * @namespace be.wl.TypeScriptDemo.controller
  */
 export default class AppController extends BaseController {
-	private northwindState:NorthwindState;
+	private northwindState: NorthwindState;
 
-	public onInit() : void {
+	public onInit(): void {
 		// apply content density mode to root view
 		this.getView().addStyleClass((this.getOwnerComponent() as AppComponent).getContentDensityClass());
 
 		this.northwindState = (this.getOwnerComponent() as AppComponent).northwindState;
-		this.setModel(this.northwindState.getModel(),"state");
-		
+		this.setModel(this.northwindState.getModel(), "state");
+
 		this.northwindState.getSupplier(0);
 	}
 
-	public sayHello() : void {
+	public sayHello(): void {
 		MessageBox.show("Hello World!");
 	}
+	public async openDialog(): Promise<void> {
+		const closeResult = (await this.openFragment({
+			name: "be.wl.TypeScriptDemo.view.dialog.About",
+			data: { param: "demo dialog" }
+		}) as string);
+		MessageToast.show(closeResult);
+	}
 
-	public async onSaveSupplier(){
+	public async onSaveSupplier() {
 		BusyIndicator.show(0);
 		try {
 			await this.northwindState.saveSupplier();
 		} catch (error) {
-			MessageBox.show(error?.message);
+			let e = (error as Error);
+			if(e.message !== undefined){
+				MessageBox.show(e.message);
+			}
 			console.error(error);
-		}finally{
+		} finally {
 			BusyIndicator.hide();
 		}
 	}
